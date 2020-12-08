e5b7663b27fd4aadb328b53a440d1d12946783bf
kangsan <piopiop1178@gmail.com>
2020-12-08 21:01:04 +0900

    commit on 'ver1'

--- app.py
+++ app.py
@@ -4,12 +4,9 @@ import requests
 
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+client = MongoClient('mongodb://test:test@localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.session  # 'dbsparta'라는 이름의 db를 만들거나 사용합니다.
 
-db = client.week1  # 'dbsparta'라는 이름의 db를 만들거나 사용합니다.
-
-# class User():
-#     id = db.Column(db.Interger,primar)
 
 @app.route('/')
 def home():
@@ -24,17 +21,27 @@ def home():
 
 @app.route('/login', methods=['POST'])
 def login():
-    id_receive = request.form['id_give']  
-    password_receive = request.form['password_give']  
+    username = request.form['id_give']
+    password = request.form['password_give']  
 
-    user = db.users.find_one({'id':id_receive})
+    user = db.session.find_one({'username':username})
     if user:
-        if password_receive == user['password']:
+        if password == user['password']:
             session['logged_in'] = True
-            session['name'] = user['id']
+            session['name'] = user['username']
             return jsonify({'result': 'success'})
             
     return jsonify({'result': 'fail'})
+
+@app.route('/register/')
+def register():
+    return render_template('register.html')
+
+@app.route('/register/add', methods=['POST'])
+def add_user():
+    new_user = {'username':request.form['id_give'], 'password':request.form['password_give']}
+    db.session.insert_one(new_user)
+    return jsonify({'result': 'success'})
 
 @app.route('/home', methods=['POST'])
 def post_memo():
@@ -47,10 +54,11 @@ def post_memo():
     return jsonify({'result': 'success'})
 
 
-# @app.route('/home', methods=['GET'])
-# def read_memos():
-#     result = list(db.memos.find({}, {'_id': 0}))
-#     return jsonify({'result': 'success', 'memos': result})
+
+@app.route('/home', methods=['GET'])
+def read_memos():
+    result = list(db.memos.find({}, {'_id': 0}))
+    return jsonify({'result': 'success', 'memos': result})
     
 
 if __name__ == '__main__':  
